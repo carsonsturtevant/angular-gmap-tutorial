3d9dc4da44dc9e64e3eda5c792ff00f7daf5b5f8
carsonsturtevant <carson.sturtevant@gmail.com>
2020-03-31 21:37:06 -0600

    got map to show up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('mapContainer') gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.730610;
+  lng = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
   title = 'angular-gmap';
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+    this.mapOptions);
+   }
 }
